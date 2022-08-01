15404bab9a4f41c15c755c6f04ab49300e9ab43d
yzm-dev <yzm_dev@163.com>
2022-08-01 21:58:03 +0800

    adding some tool codes.

--- Tools/GenerateRandom.cpp
+++ Tools/GenerateRandom.cpp
@@ -12,6 +12,16 @@ void GetRandomIntArr(int a[],int start, int end, int length) {
     }
 }
 
+// 生成 [start, end] 的随机数
+// 从数组下标 1~length
+void GenerateRandomIntArr(int a[],int start, int end, int length) {
+    srand(time(nullptr));
+    a[0] = 0;
+    for (int i = 1; i <= length; ++i) {
+        a[i] = (rand() % (end - start + 1)) + start;
+    }
+}
+
 // 交换两个值
 void Swap(int &a, int &b) {
     int temp = a;
@@ -27,5 +37,12 @@ void ArrShow(int a[], int length) {
     std::cout << std::endl;
 }
 
+// 显示数组内所有值从下标1开始
+void DisplayArr(int a[], int length) {
+    for (int i = 1; i <= length; ++i) {
+        std::cout << a[i] << ", ";
+    }
+    std::cout << std::endl;
+}
 
 
--- Tools/GenerateRandom.h
+++ Tools/GenerateRandom.h
@@ -11,11 +11,18 @@
 // 生成 [start, end] 的随机数
 void GetRandomIntArr(int a[],int start, int end, int length);
 
+// 生成 [start, end] 的随机数
+// 从数组下标 1~length
+void GenerateRandomIntArr(int a[],int start, int end, int length);
+
 // 交换两个值
 void Swap(int &a, int &b);
 
 // 显示数组内所有值
 void ArrShow(int a[], int length);
+
+// 显示数组内所有值从下标1开始
+void DisplayArr(int a[], int length);
 
 
 #endif //C_PROJECT_GENERATERANDOM_H
